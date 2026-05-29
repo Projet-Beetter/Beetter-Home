2226b83c625236771ba913e3475a69e8f4e5d9c5
Xem0nyy <anthonyazpro@gmail.com>
2026-05-29 12:24:12 +0200

    stash : idk

--- app/blueprints/beehives/routes.py
+++ app/blueprints/beehives/routes.py
@@ -5,25 +5,7 @@ from ..utils.influxdb import query_chart_data, query_latest_values, RANGE_OPTION
 from ..utils.decorators import editor_required
 from .forms import BeehiveForm
 from . import beehives_bp
-import urllib.request, urllib.parse, json
-
-def geocode(street, city, postal_code):
-    parts = [p for p in [street, postal_code, city] if p]
-    if not parts:
-        return None, None
-    try:
-        params = urllib.parse.urlencode({'q': ', '.join(parts), 'format': 'json', 'limit': 1})
-        url = f'https://nominatim.openstreetmap.org/search?{params}'
-        req = urllib.request.Request(url, headers={'User-Agent': 'Beetter/1.0'})
-        with urllib.request.urlopen(req, timeout=5) as r:
-            data = json.loads(r.read())
-        if data:
-            return float(data[0]['lat']), float(data[0]['lon'])
-    except Exception:
-        pass
-    return None, None
-
-
+from ..utils.geocode import geocode
 
 @beehives_bp.route('/')
 @login_required
